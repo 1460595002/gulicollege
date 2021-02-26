2296b28a5f7e4618140709f55bbf7bdcc91f1dd3
gjr146 <1460595002@qq.com>
2021-02-26 22:27:52 +0800

    sql脚本

--- service/service-edu/src/main/java/cn/jinronga/serviceedu/EduApplication.java
+++ service/service-edu/src/main/java/cn/jinronga/serviceedu/EduApplication.java
@@ -3,6 +3,7 @@ package cn.jinronga.serviceedu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @ComponentScan(basePackages = "cn.jinronga")
+@CrossOrigin
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
